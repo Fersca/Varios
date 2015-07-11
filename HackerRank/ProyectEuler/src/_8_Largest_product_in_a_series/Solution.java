0e99484980776c0f26a0164becb36421d2d8543d
Fernando Scasserra <fersca@hotmail.com>
2015-07-10 22:54:05 -0300

    pruebas

--- HackerRank/ProyectEuler/src/_8_Largest_product_in_a_series/Solution.java
+++ HackerRank/ProyectEuler/src/_8_Largest_product_in_a_series/Solution.java
@@ -13,19 +13,49 @@ public class Solution {
     
 	private void run() {		
 		String[] data = leer();
-		for (int i=0;i<data.length;i=i+2) {			
+		for (int i=0;i<data.length;i++) {			
 			String[] datos = data[i].split(" ");
 			int k = Integer.parseInt(datos[1]);
 			i++;
 			String n = data[i];
-			procesar(k,n);
+			procesarPeor(k,n);
 		}
 	}
-			
-	private void procesar(int k, String n) {
 
+	private void procesarPeor(int k, String n) {
+				
+		/* Esto anda pero no es muy performante,
+		 * Lo que se puede hacer es multiplicar por el siguiente numero y dividir por el primero
+		 * el tema con eso es que si el primero es un cero no hay que dividir por él,
+		 * con lo cual lo que hay que hacer es ir detectando cuando hay un cero y avanzar
+		 * 
+		 */
+		char[] numeros = n.toCharArray();
 		
-		//TODO: hay que arreglar el tema de cuando se tienen un cero.
+		long max = 0;
+		
+		//recorro todos las ventanas
+		for (int i=0;i<numeros.length-k;i++) {
+
+			//calculo la multiplicacion de la ventana
+			long mult = 1;
+			for (int j = 0;j<k;j++) {
+				int num = Integer.parseInt(""+numeros[i+j]);
+				mult = mult * num;				
+			}
+			
+			//me guardo el máximo
+			if (mult>max)
+				max = mult;
+			
+		}
+		  
+		System.out.println(max);
+	}
+
+	private void procesar(int k, String n) {
+		
+		//TODO: hay que arreglar el tema de cuando se tienen un cero.				
 		
 		char[] numeros = n.toCharArray();
 		
