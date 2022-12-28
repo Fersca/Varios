582939dff6f36ed99dd1fa23cf1c68569808d287
Fernando Scasserra <fernando.scasserra@kavak.com>
2022-12-28 15:54:45 -0300

    Quarkus examples

---
+++ quarkus/rest-json-quickstart/src/main/java/org/acme/rest/json/Fruit.java
@@ -0,0 +1,19 @@
+package org.acme.rest.json;
+
+/**
+ *
+ * @author Fernando.Scasserra
+ */
+public class Fruit {
+
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
