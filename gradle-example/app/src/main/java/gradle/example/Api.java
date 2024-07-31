115269696b36b98d5591ca5e7abbb1d42c538d6f
Fernando Scasserra <fersca@hotmail.com>
2024-07-31 00:24:47 -0300

    definición del json

---
+++ gradle-example/app/src/main/java/gradle/example/Api.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gradle.example;
+
+/**
+ *
+ * @author fersc
+ */
+public class Api {
+    
+    public static void main(String[] args) {
+        
+        //Leer el archivo de configuración.
+        
+        //Crear el webserver
+        
+        //setear las url's en el webserver con sus respectivos metodos de código interpretado
+        
+        
+        
+    }
+    
+}
